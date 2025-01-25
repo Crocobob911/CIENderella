2f5512c62f80ea670408c1aa4e8ba3e230506632
Crocobob <mjdaniel911@naver.com>
2025-01-25 23:56:43 +0900

    Project On Board
    가보자 가보자~

---
+++ src/test/java/com/crocobob/CIENderella/CieNderellaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crocobob.CIENderella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CieNderellaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
