0a12c577684d003c509beb1ac26c044380cb3606
crocobob911 <mjdaniel911@naver.com>
2025-03-14 15:03:25 +0900

    docker volume set : update

--- src/main/java/crocobob/SISO/SISOCommandLineRunner.java
+++ src/main/java/crocobob/SISO/SISOCommandLineRunner.java
@@ -35,25 +35,25 @@ public class SISOCommandLineRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         // For Test
-        contentRepo.save(new Content(21, 23,
-                "cien14789*", true,
-                "{month}/{day}_CIEN 사용신청입니다.",
-                "동아리명: CIEN\n" +
-                        "사용 날짜 및 시간 :{next_month}월 {next_day}일 00시~07시\n" +
-                        "신청자 : {writer}\n" +
-                        "사유 : {reason}"));
+//        contentRepo.save(new Content(21, 23,
+//                "cien14789*", true,
+//                "{month}/{day}_CIEN 사용신청입니다.",
+//                "동아리명: CIEN\n" +
+//                        "사용 날짜 및 시간 :{next_month}월 {next_day}일 00시~07시\n" +
+//                        "신청자 : {writer}\n" +
+//                        "사유 : {reason}"));
 
-        reasonRepo.save(new Reason("게임 개발 회의", true));
-        reasonRepo.save(new Reason("신입생 면접 준비", true));
+//        reasonRepo.save(new Reason("게임 개발 회의", true));
+//        reasonRepo.save(new Reason("신입생 면접 준비", true));
 
-        writerRepo.save(new Writer("이유정", true));
-        writerRepo.save(new Writer("김현우", true));
-        writerRepo.save(new Writer("노영욱", true));
-        writerRepo.save(new Writer("석재혁", true));
-        writerRepo.save(new Writer("김준", true));
+//        writerRepo.save(new Writer("이유정", true));
+//        writerRepo.save(new Writer("김현우", true));
+//        writerRepo.save(new Writer("노영욱", true));
+//        writerRepo.save(new Writer("석재혁", true));
+//        writerRepo.save(new Writer("김준", true));
 
-        mealService.createWeeklyMealData();
+//        mealService.createWeeklyMealData();
 
-        galRepo.save(new GalleryInfo(5, 10, 5));
+//        galRepo.save(new GalleryInfo(5, 10, 5));
     }
 }
